380d8adbc390a2ee34d84ddfeb9e22fe6281ba58
TestDevelopeer <testdeveloper7967@gmail.com>
2025-06-15 19:58:49 +0300

    вынес поиск пользователя в команду, сюда отдаю только токен

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -2,21 +2,24 @@
 
 namespace App\Services;
 
-use App\Models\User;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 
 class TelegramService
 {
-    protected $user;
+    protected string $token;
 
-    public function __construct($userId)
+    public function __construct($token)
     {
-        $this->user = User::find($userId);
+        $this->token = $token;
     }
 
+    /**
+     * @throws ConnectionException
+     */
     public function sendNotification($message)
     {
-        return Http::api($this->user->apiToken())->post('/telegram/send/message', [
+        return Http::api($this->token)->post('/telegram/send/message', [
             'message' => $message,
         ]);
     }
@@ -26,17 +29,29 @@ class TelegramService
         $message = "<b>Новая запись в журнале взвешиваний</b>\n";
         $message .= "--------------------------------\n";
         $message .= "<b>Номер документа:</b> {$record['DOCUMENT_NUMBER']}\n";
-        $message .= "<b>Дата/время:</b> {$record['WEIGHING_DATETIME']}\n";
+        $message .= "<b>Дата/время создания записи:</b> {$record['DATETIME_CREATE']}\n";
+        $message .= "--------------------------------\n";
+        if ($record['TRUCKDRIVER']) {
+            $message .= "<b>Водитель:</b> {$record['TRUCKDRIVER']}\n";
+        }
+        $message .= "<b>Тип ТС:</b> {$record['TS_TYP_NAME']}\n";
+        $message .= "<b>Номер ТС:</b> {$record['FULL_NUMB_TS']}\n";
+        if ($record['FULL_NUMB_TRAILER']) {
+            $message .= "<b>Номер прицепа:</b> {$record['FULL_NUMB_TRAILER']}\n";
+        }
+        $message .= "--------------------------------\n";
         $message .= "<b>Тип взвешивания:</b> {$record['TYPWEIGHINGCAPTION']}\n";
-        $message .= "<b>Транспорт:</b> {$record['FULL_NUMB_TS']}\n";
-        $message .= "<b>Прицеп:</b> {$record['FULL_NUMB_TRAILER']}\n";
-        $message .= "<b>Груз:</b> {$record['CARGO_NAME']}\n";
-        $message .= "<b>Отправитель:</b> {$record['SENDER_NAME']}\n";
-        $message .= "<b>Получатель:</b> {$record['RECEIVER_NAME']}\n";
         $message .= "<b>Брутто:</b> {$record['BRUTTO']} {$record['UNITMEAS']}\n";
         $message .= "<b>Тара:</b> {$record['TARA']} {$record['UNITMEAS']}\n";
         $message .= "<b>Нетто:</b> {$record['NETTO']} {$record['UNITMEAS']}\n";
         $message .= "<b>Весы:</b> {$record['SCALE_NAME']}\n";
+        $message .= "--------------------------------\n";
+        $message .= "<b>Груз:</b> {$record['CARGO_NAME']}\n";
+        $message .= "<b>Отправитель:</b> {$record['SENDER_NAME']}\n";
+        $message .= "<b>Получатель:</b> {$record['RECEIVER_NAME']}\n";
+        if ($record['CARRIER_NAME']) {
+            $message .= "<b>Перевозчик:</b> {$record['CARRIER_NAME']}\n";
+        }
 
         return $message;
     }
