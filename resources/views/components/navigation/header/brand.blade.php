ad034f13caea86c3fb2734377d09011120b5b97b
TestDevelopeer <testdeveloper7967@gmail.com>
2025-06-15 19:57:25 +0300

    название проекта

--- resources/views/components/navigation/header/brand.blade.php
+++ resources/views/components/navigation/header/brand.blade.php
@@ -2,7 +2,7 @@
 <div class="brand">
     <a href="{{ route('dashboard') }}" class="brand-logo w-100">
         <iconify-icon icon="lets-icons:time-progress-duotone" class="fs-24px me-2 text-theme"></iconify-icon>
-        <span class="brand-text fw-500 fs-14px">АСП UNISERVER</span>
+        <span class="brand-text fw-500 fs-14px">АСП UNISERVER CLIENT</span>
     </a>
 </div>
 <!-- END brand -->
