13e38a8df7df88ddcccd4be88f6a9b3f5cf83039
TestDevelopeer <testdeveloper7967@gmail.com>
2025-06-15 19:59:24 +0300

    убрал данные uniserver пользователя

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'uniserver_name',
-        'uniserver_password',
         'remember_token',
         'password',
     ];
