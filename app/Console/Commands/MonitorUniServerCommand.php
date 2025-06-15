ca080595e2ef3fe9df8e41b33f691f6f247fd006
TestDevelopeer <testdeveloper7967@gmail.com>
2025-06-15 20:00:19 +0300

    Добавил получение и использование данных пользователя из главной БД, различные уведомления и ошибки в консоли

--- app/Console/Commands/MonitorUniServerCommand.php
+++ app/Console/Commands/MonitorUniServerCommand.php
@@ -2,10 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Models\User;
+use App\Services\ProfileService;
 use App\Services\TelegramService;
 use App\Services\UniServerService;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\Isolatable;
+use Illuminate\Http\Client\ConnectionException;
 
 class MonitorUniServerCommand extends Command implements Isolatable
 {
@@ -13,24 +16,67 @@ class MonitorUniServerCommand extends Command implements Isolatable
 
     protected $description = 'Monitor UniServer API for new records';
 
+    /**
+     * @throws ConnectionException
+     */
     public function handle(): void
     {
         $userId = $this->option('user');
 
-        $uniServer = new UniServerService($userId);
-        $telegram = new TelegramService($userId);
+        $this->info("Получение информации о пользователе #$userId...");
 
-        $this->info("Starting UniServer monitoring for user {$userId}...");
+        $user = User::find($userId);
 
-        while (true) {
-            $newRecord = $uniServer->checkForNewRecords();
-            if ($newRecord) {
-                $message = $telegram->formatRecordMessage($newRecord);
-                $telegram->sendNotification($message);
-                $this->info('New record detected and notification sent!');
+        if ($user) {
+            $token = $user->apiToken();
+
+            $profileService = new ProfileService($token);
+
+            $response = $profileService->getProfileInfo();
+
+            if ($response->successful()) {
+                $this->info("Информация о пользователе #$userId получена!");
+
+                $data = $response->json();
+
+                $user = $data['user'];
+                $user['uniserver_user'] = $data['uniserver_user'];
+                $user['telegram'] = $data['telegram'];
+
+                if ($user['uniserver_user'] === [] || $user['uniserver_user']['name'] === null || $user['uniserver_user']['password'] === null) {
+                    $this->error("Аккаунт Uniserver не закреплен за пользователем #$userId. Обратитесь к администратору!");
+                    return;
+                }
+
+                if ($user['telegram'] === [] || $user['telegram']['chat_id'] === null) {
+                    $this->error("Аккаунт Telegram не закреплен за пользователем #$userId. Обратитесь к администратору!");
+                    return;
+                }
+
+
+                $this->info("Начинаю мониторинг UniServer для пользователя #$userId...");
+
+                $uniServerService = new UniServerService($token, $user['uniserver_user']);
+                $uniServerService->setLastRecordCode();
+
+                $telegramService = new TelegramService($token);
+
+                while (true) {
+                    $newRecord = $uniServerService->checkForNewRecords();
+                    if ($newRecord) {
+                        $message = $telegramService->formatRecordMessage($newRecord);
+                        $telegramService->sendNotification($message);
+                        $this->newLine();
+                        $this->info("Найдена новая запись в журнале с кодом: {$newRecord['CODE']}! Уведомление отправлено в Telegram!");
+                        $this->newLine();
+                        $this->info("Продолжаю мониторинг UniServer для пользователя #$userId...");
+                    }
+
+                    sleep(5); // Проверяем каждые 5 секунд
+                }
+            } else {
+                $this->error("Не удалось получить информацию о пользователе #$userId. Обратитесь к администратору!");
             }
-
-            sleep(5); // Проверяем каждые 5 секунд
         }
     }
 }
